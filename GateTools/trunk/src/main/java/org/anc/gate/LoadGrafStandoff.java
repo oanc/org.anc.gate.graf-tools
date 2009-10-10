542bd08b48a0e40a3e5b035b71ee5ecffff0153a
ksuderman <ksuderman@edd201f6-e76c-4490-bd89-b6241560e6fd>
2009-10-10 17:50:56 +0000

    Updated copyright notices on LoadGrafStandoff and SaveGrafStandoff.

--- GateTools/trunk/src/main/java/org/anc/gate/LoadGrafStandoff.java
+++ GateTools/trunk/src/main/java/org/anc/gate/LoadGrafStandoff.java
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2008 American National Corpus
+ * Copyright (c) 2009 American National Corpus
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
