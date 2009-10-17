6c7b2c81b82c19b13769d8e6fbb6f18bab7bf8d7
Keith Suderman <ksuderman@users.noreply.github.com>
2009-10-17 15:02:51 +0000

    LoadGrafStandoff now checks offsets to ensure they do not extends past the end of the content.

--- copy-to-gate.bat
+++ copy-to-gate.bat
@@ -3,9 +3,24 @@ setlocal
 
 if "%GATE_HOME%"=="" echo no-gate
 
+if "%COMPUTERNAME%"=="SCOTTY" goto scotty:
+
 set DIR=%GATE_HOME%\Plugins\ANC
+goto run
+
+:scotty
+set DIR=d:\dev\GatePlugins\ANC
+goto run
+
+:run
+echo on
 copy target\ANC-GATE.jar %DIR%
 copy src\main\resources\creole.xml %DIR%
+@echo off
+goto end
 
 :no-gate
+echo GATE_HOME not set.
+
+:end
 endlocal
